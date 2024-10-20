a8d689476065f1fc9d4d21435a63c82f4d0685af
Diversiam <jerry2912@icloud.com>
2024-10-20 16:23:35 +0200

    Setup MoveHelper initial state

--- src/Moves/MoveHelper.cpp
+++ src/Moves/MoveHelper.cpp
@@ -8,3 +8,57 @@
   ==============================================================================
 */
 
+
+#include "MoveHelper.h"
+
+
+MoveHelper::MoveHelper()
+{
+}
+
+
+MoveHelper::~MoveHelper()
+{
+}
+
+
+bool MoveHelper::checkAvailableMoves(PieceType piece)
+{
+	return false;
+}
+
+
+std::vector<Move> MoveHelper::getAvailableMoves()
+{
+	return std::vector<Move>();
+}
+
+
+bool MoveHelper::checkDiagonalMoves()
+{
+	return false;
+}
+
+
+bool MoveHelper::checkAdjacentMoves()
+{
+	return false;
+}
+
+
+bool MoveHelper::checkLinearForwardMove()
+{
+	return false;
+}
+
+
+bool MoveHelper::checkPawnCaptureMoves()
+{
+	return false;
+}
+
+
+bool MoveHelper::checkLShapedMoves()
+{
+	return false;
+}
--- src/Moves/MoveHelper.h
+++ src/Moves/MoveHelper.h
@@ -10,7 +10,54 @@
 
 #pragma once
 
+#include <vector>
+#include <utility>
+#include <array>
+
+#include "Move.h"
+
+
+enum MoveType
+{
+	Diagonal = 1,  // Bishop, Queen
+	LinearForward, // Pawns moving
+	Adjacent,	   // King moving one step in any direction
+	Vertical,	   // Rook, Queen moving along vertical lines (files)
+	LShaped		   // Knight's jump
+};
+
 
 class MoveHelper
 {
+public:
+	MoveHelper();
+
+	~MoveHelper();
+
+
+	bool checkAvailableMoves(PieceType piece);
+
+	std::vector<Move> getAvailableMoves();
+
+
+private:
+	bool							   checkDiagonalMoves();
+
+	bool							   checkAdjacentMoves();
+
+	bool							   checkLinearForwardMove();
+
+	bool							   checkPawnCaptureMoves();
+
+	bool							   checkLShapedMoves();
+
+
+	std::array<std::pair<int, int>, 8> mAdjacentPositions  = {{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}}};
+
+	std::array<std::pair<int, int>, 4> mDiagonalDirections = {{{1, 1}, {1, -1}, {-1, 1}, {-1, -1}}};
+
+	std::array<std::pair<int, int>, 8> mLShapedDirections  = {{{1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}}};
+
+
+	std::vector<Move>				   mPossibleMovesAndCaptures;
 };

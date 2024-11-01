e536a5aff1a6a32ccf188c4051f85c3dcfb2837e
Diversiam <jerry2912@icloud.com>
2024-11-01 17:43:16 +0100

    Removed comments

--- src/Moves/MovementManager.h
+++ src/Moves/MovementManager.h
@@ -22,13 +22,6 @@ class MovementManager
 public:
 	MovementManager(ChessBoard &board);
 
-	//bool isValidMove(const Move &move, PieceColor playerColor);
-	//bool executeMove(const Move &move);
-
-	//bool isKingInCheck(PieceColor color);
-	//bool isCheckmate(PieceColor color);
-	//bool isStalemate(PieceColor color);
-
 	std::unordered_map<Position, std::vector<PossibleMove>> getAllPossibleMoves(PieceColor playerColor);
 
 private:
@@ -38,17 +31,4 @@ private:
 	
 	bool		isSquareAttacked(const Position &square, PieceColor attackerColor);
 
-
-	//bool		wouldKingBeInCheckAfterMove(const Move &move, PieceColor color);
-	//bool		isSquareUnderAttack(int x, int y, PieceColor color);
-
-	//bool		isCastlingMove(const Move &move, PieceColor color);
-	//bool		validateCastling(const Move &move, PieceColor color);
-	//void		performCastling(const Move &move);
-
-	//bool		isEnPassantMove(const Move &move, PieceColor color);
-	//bool		validateEnPassant(const Move &move, PieceColor color);
-	//void		performEnPassant(const Move &move);
-
-	//void		promotePawn(int x, int y, PieceColor color, PieceType promotionType);
 };

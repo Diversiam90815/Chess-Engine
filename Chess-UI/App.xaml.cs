cb80ce229c5cbff5d1dd85f0ef9e6b1dcb6897a5
Jens W. Langenberg <jerry2912@icloud.com>
2024-12-15 14:50:35 +0100

    Added baisc pieces and board assets

--- Chess-UI/App.xaml.cs
+++ Chess-UI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Dispatching;
+﻿using Chess_UI.Configuration;
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -42,7 +43,7 @@ namespace Chess_UI
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
+            m_window = new MainMenuWindow();
 
             ChessLogicAPI.Init();
 

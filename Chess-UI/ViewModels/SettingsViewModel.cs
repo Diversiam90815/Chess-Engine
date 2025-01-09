80ad917217cd54ba7ae81daf25d52f8f564998d6
Jens W. Langenberg <jerry2912@icloud.com>
2025-01-09 21:04:58 +0100

    Added View, ViewModel of settings window

---
+++ Chess-UI/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Dispatching;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Chess_UI.Services;
+
+
+namespace Chess_UI.ViewModels
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly DispatcherQueue DispatcherQueue;
+
+        private Configuration Configuration;
+
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            DispatcherQueue.TryEnqueue(() =>
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            });
+        }
+    }
+}

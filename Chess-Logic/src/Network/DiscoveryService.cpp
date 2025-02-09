15dcb2fd4aa2df2143e4f3c16330f881a80bf317
Jens W. Langenberg <jerry2912@icloud.com>
2025-02-09 16:35:36 +0100

    Created placeholder methods in DiscoveryService

--- Chess-Logic/src/Network/DiscoveryService.cpp
+++ Chess-Logic/src/Network/DiscoveryService.cpp
@@ -6,3 +6,33 @@
 */
 
 #include "DiscoveryService.h"
+
+
+DiscoveryService::DiscoveryService() {}
+
+
+DiscoveryService::~DiscoveryService() {}
+
+
+void DiscoveryService::init(asio::io_context &io_context, unsigned short tcpPort, const std::string &playerName) {}
+
+
+void DiscoveryService::start() {}
+
+
+void DiscoveryService::stop() {}
+
+
+void DiscoveryService::setPeerCallback(PeerCallback callback) {}
+
+
+void DiscoveryService::sendPackage(std::string IPAddress) {}
+
+
+void DiscoveryService::handleSend(const boost::system::error_code &error, size_t bytesSent) {}
+
+
+void DiscoveryService::startReceive() {}
+
+
+void DiscoveryService::handleReceive(const boost::system::error_code &error, size_t bytesReceived) {}
--- Chess-Logic/src/Network/DiscoveryService.h
+++ Chess-Logic/src/Network/DiscoveryService.h
@@ -7,6 +7,89 @@
 
 #pragma once
 
+#include <boost/asio.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/ip/udp.hpp>
+
+#include <functional>
+#include <string>
+#include <json.hpp>
+
+
+using json	   = nlohmann::json;
+namespace asio = boost::asio;
+using boost::asio::ip::udp;
+
+using PeerCallback = std::function<void(const json &)>;
+
+
+struct RemoteEndpoint
+{
+	std::string IPAddress{};
+	int			tcpPort{0};
+	std::string playerName{};
+};
+
+
+// Enable automatic JSON conversion.
+inline void to_json(json &j, const RemoteEndpoint &ep)
+{
+	j = json{{"IPAddress", ep.IPAddress}, {"tcpPort", ep.tcpPort}, {"playerName", ep.playerName}};
+}
+
+inline void from_json(const json &j, RemoteEndpoint &ep)
+{
+	j.at("IPAddress").get_to(ep.IPAddress);
+	j.at("tcpPort").get_to(ep.tcpPort);
+	j.at("playerName").get_to(ep.playerName);
+}
+
+
 class DiscoveryService
 {
+public:
+	DiscoveryService();
+	~DiscoveryService();
+
+	void init(asio::io_context &io_context, unsigned short tcpPort, const std::string &playerName);
+
+	void start();
+
+	void stop();
+
+	void setPeerCallback(PeerCallback callback);
+
+private:
+	void						sendPackage(std::string IPAddress = "255.255.255.255");
+
+	void						handleSend(const boost::system::error_code &error, size_t bytesSent);
+
+	void						startReceive();
+
+	void						handleReceive(const boost::system::error_code &error, size_t bytesReceived);
+
+
+	const int					mDiscoveryPort = 5555; // needs to be set from config later
+
+	int							tcpPort{0};
+
+	std::string					mPlayerName{};
+
+
+
+	std::vector<RemoteEndpoint> mRemoteDevices;
+
+	PeerCallback				mPeerCallback;
+
+	std::atomic<bool>			mInitialized{false};
+	std::atomic<bool>			mIsRunning{false};
+
+
+	asio::io_context		   *mIoContext = nullptr;
+
+	udp::socket					mSocket;
+
+	udp::endpoint				mSenderEndpoint;
+
+	std::array<char, 1024>		mRecvBuffer;
 };

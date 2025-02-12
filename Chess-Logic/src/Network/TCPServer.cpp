adc60397b461c21508392576b8ccf34e564212ad
Jens W. Langenberg <jerry2912@icloud.com>
2025-02-12 20:24:38 +0100

    Added blueprint methods to TCPServer from example

--- Chess-Logic/src/Network/TCPServer.cpp
+++ Chess-Logic/src/Network/TCPServer.cpp
@@ -6,3 +6,24 @@
 */ 
 
 #include "TCPServer.h"
+
+
+TCPServer::TCPServer() {}
+
+
+TCPServer::~TCPServer() {}
+
+
+void TCPServer::init(boost::asio::io_context &ioContext, unsigned short port) {}
+
+
+void TCPServer::startAccept() {}
+
+
+void TCPServer::setMessageHandler() {}
+
+
+void TCPServer::handleAccept(std::shared_ptr<tcp::socket> socket, const boost::system::error_code &error) {}
+
+
+void TCPServer::doRead(std::shared_ptr<tcp::socket> socket) {}
--- Chess-Logic/src/Network/TCPServer.h
+++ Chess-Logic/src/Network/TCPServer.h
@@ -3,12 +3,43 @@
 	Class:          TCPServer
 	Description:    Server implementation used for the multiplayer mode
   ==============================================================================
-*/ 
+*/
 
 
 #pragma once
 
+#include <boost/asio.hpp>
+#include <functional>
+#include <json.hpp>
+
+
+using json = nlohmann::json;
+using boost::asio::ip::tcp;
+
+using MessageHandler = std::function<void(const json &message, std::shared_ptr<tcp::socket> socket)>;
+
 
 class TCPServer
 {
+	TCPServer();
+	~TCPServer();
+
+	void init(boost::asio::io_context &ioContext, unsigned short port);
+
+	void startAccept();
+	
+	void setMessageHandler();
+
+
+private:
+	void					 handleAccept(std::shared_ptr<tcp::socket> socket, const boost::system::error_code &error);
+	
+	void					 doRead(std::shared_ptr<tcp::socket> socket);
+
+
+	boost::asio::io_context &mIoContext;
+
+	tcp::acceptor			 mAcceptor;
+	
+	MessageHandler			 mMessageHandler;
 };

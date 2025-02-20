17868aa615155b7f300aee3b9370dc9a798c53f9
Jens W. Langenberg <jerry2912@icloud.com>
2025-02-20 16:23:41 +0100

    Added Interface classes for Observers/Observables

--- Chess-Logic/src/Observer/IObservable.h
+++ Chess-Logic/src/Observer/IObservable.h
@@ -1,1 +1,49 @@
+/*
+  ==============================================================================
+	Module:			IObserver
+	Description:    Interface observer class used by the Observer Pattern
+  ==============================================================================
+*/
+
 #pragma once
+
+#include "IObserver.h"
+#include "Move.h"
+
+
+class IPlayerObservable
+{
+public:
+	virtual ~IPlayerObservable() {};
+	virtual void attachObserver(IPlayerObserver *observer);
+	virtual void detachObserver(IPlayerObserver *observer);
+
+	virtual void updateScore()												 = 0;
+	virtual void playerChanged(PlayerColor player)							 = 0;
+	virtual void addCapturedPiece(PlayerColor player, PieceType captured)	 = 0;
+	virtual void remoteCapturedPiece(PlayerColor player, PieceType captured) = 0;
+};
+
+
+class IMoveObservable
+{
+public:
+	virtual ~IMoveObservable() {};
+	virtual void attachObserver(IMoveObserver *observer);
+	virtual void detachObserver(IMoveObserver *observer);
+
+	virtual void executeMove(PossibleMove &move) = 0;
+	virtual void addMoveToHistory(Move &move)	 = 0;
+};
+
+
+class IGameObservable
+{
+public:
+	virtual ~IGameObservable() {};
+	virtual void attachObserver(IGameObserver *observer);
+	virtual void detachObserver(IGameObserver *observer);
+
+	virtual void endGame(PlayerColor winner)	   = 0;
+	virtual void gameStateChanged(GameState state) = 0;
+};
--- Chess-Logic/src/Observer/IObserver.h
+++ Chess-Logic/src/Observer/IObserver.h
@@ -1,1 +1,42 @@
+/*
+  ==============================================================================
+	Module:			IObserver
+	Description:    Interface observer class used by the Observer Pattern
+  ==============================================================================
+*/
+
 #pragma once
+
+#include "Move.h"
+
+
+class IPlayerObserver
+{
+public:
+	virtual ~IPlayerObserver() {};
+
+	virtual void onScoreUpdate()											   = 0;
+	virtual void omPlayerChanged(PlayerColor player)						   = 0;
+	virtual void onAddCapturedPiece(PlayerColor player, PieceType captured)	   = 0;
+	virtual void onRemoteCapturedPiece(PlayerColor player, PieceType captured) = 0;
+};
+
+
+class IMoveObserver
+{
+public:
+	virtual ~IMoveObserver() {};
+
+	virtual void onExecuteMove(PossibleMove &move) = 0;
+	virtual void onAddToMoveHistory(Move &move)	   = 0;
+};
+
+
+class IGameObserver
+{
+public:
+	virtual ~IGameObserver() {};
+
+	virtual void onGameStateChanged(GameState state) = 0;
+	virtual void onEndGame(PlayerColor winner) = 0;
+};

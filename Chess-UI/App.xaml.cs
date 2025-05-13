aca3523c41b85d6d838fe9c4194abb481a685d9c
Jens W. Langenberg <jerry2912@icloud.com>
2025-05-13 20:57:36 +0200

    Started implementing DI

--- Chess-UI/App.xaml.cs
+++ Chess-UI/App.xaml.cs
@@ -1,5 +1,15 @@
-﻿using Chess_UI.Services;
+﻿using Chess_UI.Board;
+using Chess_UI.Coordinates;
+using Chess_UI.Models.Interfaces;
+using Chess_UI.Moves;
+using Chess_UI.Services;
+using Chess_UI.Services.Interfaces;
+using Chess_UI.Themes;
+using Chess_UI.Themes.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
+using System;
+using System.Security.Authentication.ExtendedProtection;
 
 
 namespace Chess_UI
@@ -8,14 +18,18 @@ namespace Chess_UI
     {
         private Window MainMenu;
 
-        public ChessLogicCommunicationLayer ChessLogicCommunication { get; set; }
+        public CommunicationLayer ChessLogicCommunication { get; set; }
 
         public static new App Current { get; private set; }
+
+        public IServiceProvider Services { get; }
 
 
         public App()
         {
-            ChessLogicCommunication = new ChessLogicCommunicationLayer();
+            Services = ConfigureServices();
+
+            ChessLogicCommunication = new CommunicationLayer();
             Current = this;
             this.InitializeComponent();
         }
@@ -35,6 +49,25 @@ namespace Chess_UI
             MainMenu.Activate();
 
             Logger.LogInfo("App initialized!");
+        }
+
+        
+        private IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Register Services
+            services.AddSingleton<IChessCoordinate, ChessCoordinate>();
+            services.AddSingleton<IThemeManager, ThemeManager>();
+            services.AddTransient<IMoveModel, MoveModel>();
+            services.AddTransient<IBoardModel, BoardModel>();
+
+            // Register View Models
+            services.AddTransient<ViewModels.ChessBoardViewModel>();
+            services.AddTransient<ViewModels.ScoreViewModel>();
+            services.AddTransient<ViewModels.MoveHistoryViewModel>();
+
+            return services.BuildServiceProvider();
         }
     }
 }

436018fd7687924d0987e9b29af8fd6dfbca941f
Jens Langenberg <jens.langenberg@corsair.com>
2025-07-09 19:35:33 +0200

    Added getter/setter to PlayerName and get PC name as default

--- Chess.Engine/Chess.Engine/src/Multiplayer/PlayerName/PlayerName.cpp
+++ Chess.Engine/Chess.Engine/src/Multiplayer/PlayerName/PlayerName.cpp
@@ -7,3 +7,32 @@
 
 #include "PlayerName.h"
 
+
+void PlayerName::init() {}
+
+
+void PlayerName::setLocalPlayerName(const std::string &name){mLocalPlayerName = name;
+}
+
+
+std::string PlayerName::getLocalPlayerName()
+{
+	return mLocalPlayerName;
+}
+
+
+std::string PlayerName::getComputerNameAsPlayerName()
+{
+	DWORD bufferSize = MAX_COMPUTERNAME_LENGTH + 1;
+	char  buffer[MAX_COMPUTERNAME_LENGTH + 1];
+
+	if (GetComputerNameA(buffer, &bufferSize))
+	{
+		return std::string(buffer, bufferSize);
+	}
+	else
+	{
+		return "Unknown";
+	}
+}
+
--- Chess.Engine/Chess.Engine/src/Multiplayer/PlayerName/PlayerName.h
+++ Chess.Engine/Chess.Engine/src/Multiplayer/PlayerName/PlayerName.h
@@ -7,9 +7,28 @@
 
 #pragma once
 
+#include <Windows.h>
+
 #include "UserSettings.h"
 
 
 class PlayerName
 {
+public:
+	PlayerName()  = default;
+	~PlayerName() = default;
+
+	void		init();
+
+	void		setLocalPlayerName(const std::string &name);
+	std::string getLocalPlayerName();
+
+private:
+	// At first startup we select the local PC name as the player name by default
+	std::string	 getComputerNameAsPlayerName();
+
+
+	std::string	 mLocalPlayerName;
+
+	UserSettings mUserSettings;
 };

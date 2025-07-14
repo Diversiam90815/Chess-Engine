7dc10151b30ef6b1b4c43cd5a282ceb01846fd08
Jens Langenberg <jens.langenberg@corsair.com>
2025-07-14 19:05:17 +0200

    Implemented IAudioEngine interface

--- Chess.UI/Chess.UI/Audio/Core/IAudioEngine.cs
+++ Chess.UI/Chess.UI/Audio/Core/IAudioEngine.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chess.UI.Audio.Core
 {
-    internal class IAudioEngine
+    public interface IAudioEngine
     {
+        // Lifecycle
+        Task InitializeAsync();
+        void Dispose();
+
+        // Module Management
+        void RegisterModule<T>(T module) where T : IAudioModule;
+        T GetModule<T>() where T : IAudioModule;
+        void UnregisterModule<T>() where T : IAudioModule;
+
+        // Global controls
+        void SetMasterVolume(float volume);
+        float GetMasterVolume();
+
+        // Events
+        event EventHandler<AudioEngineEventArgs> ModuleRegistered;
+        event EventHandler<AudioEngineEventArgs> ModuleUnregistered;
+        event EventHandler<AudioEngineEventArgs> AudioError;
     }
+
+
+    public interface IAudioModule
+    {
+
+    }
+
+    public class AudioEngineEventArgs : EventArgs
+    { }
 }

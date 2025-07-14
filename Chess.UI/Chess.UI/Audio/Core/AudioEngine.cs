a770fdc1a5f51b229b91ed7323799883b5f1303a
Jens Langenberg <jens.langenberg@corsair.com>
2025-07-14 19:30:10 +0200

    Added blank AudioEngine

--- Chess.UI/Chess.UI/Audio/Core/AudioEngine.cs
+++ Chess.UI/Chess.UI/Audio/Core/AudioEngine.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chess.UI.Audio.Core
 {
-    internal class AudioEngine
+    public class AudioEngine : IAudioEngine, IDisposable
     {
+        private readonly ConcurrentDictionary<AudioModuleType, IAudioModule> _modules;
+        private float _masterVolume = 1.0f;
+        private bool _isInitialized = false;
+
+        public event EventHandler<AudioEngineEventArgs> ModuleRegistered;
+        public event EventHandler<AudioEngineEventArgs> ModuleUnregistered;
+        public event EventHandler<AudioErrorEventArgs> AudioError;
+
+
+        public AudioEngine()
+        {
+            _modules = new ConcurrentDictionary<AudioModuleType, IAudioModule>();
+        }
+
+        public void RegisterModule(AudioModuleType type, IAudioModule moduley)
+        {
+
+        }
+
+        public T GetModule<T>(AudioModuleType moduleType) where T : class, IAudioModule
+        {
+
+        }
+
+        public IAudioModule GetModule(AudioModuleType moduleType)
+        {
+
+        }
+
+        public void UnregisterModule(AudioModuleType moduleType)
+        {
+        }
+
+        public void SetMasterVolume(float masterVolume)
+        {
+        }
+
+        public float GetMasterVolume()
+        {
+
+        }
+
+        public void Dispose()
+        {
+        }
     }
-}

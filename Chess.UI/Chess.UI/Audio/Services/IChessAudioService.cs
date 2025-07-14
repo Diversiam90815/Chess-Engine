bdb43dc9fe5a28cfd5f8f5864cf9a4387b5f16ae
Jens Langenberg <jens.langenberg@corsair.com>
2025-07-14 22:35:56 +0200

    Added higher level audio interface IChessAudioService

---
+++ Chess.UI/Chess.UI/Audio/Services/IChessAudioService.cs
@@ -0,0 +1,41 @@
+﻿using Chess.UI.Audio.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Chess.UI.Services.EngineAPI;
+
+namespace Chess.UI.Audio.Services
+{
+    public enum UIInteraction
+    {
+        ButtonClick,
+        MenuOpen,
+        ItemSelected,
+        PieceMove,
+    }
+
+
+    public interface IChessAudioService
+    {
+        Task InitializeAsync();
+
+        // Chess Game Events
+        Task HandleMoveAsync(PossibleMoveInstance move, bool isCapture);
+        Task HandleGameStateAsync(EndGameState state);
+        Task HandleCheckAsync();
+        Task HandleGameStartAsync();
+        Task HandleGameEndAsync();
+
+        // UI Interaction
+        Task HandleUIInteractionAsync(UIInteraction interaction);
+
+        // Settings
+        void SetSFXEnabled(bool enabled);
+        void SetSFXVolume(float volume);
+        void SetMasterVolume(float volume);
+
+        ISoundEffectsModule SFXModule { get; }
+    }
+}

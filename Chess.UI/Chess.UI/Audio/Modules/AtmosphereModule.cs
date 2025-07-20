6f659145f47c4dbb40eb4eb0e82c7ba1a9b97156
Jens Langenberg <jens.langenberg@corsair.com>
2025-07-20 23:32:09 +0200

    Added basic members for AtmosphereModule

--- Chess.UI/Chess.UI/Audio/Modules/AtmosphereModule.cs
+++ Chess.UI/Chess.UI/Audio/Modules/AtmosphereModule.cs
@@ -1,9 +1,12 @@
 ﻿using Chess.UI.Audio.Core;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 
 namespace Chess.UI.Audio.Modules
 {
@@ -41,7 +44,34 @@ namespace Chess.UI.Audio.Modules
         event EventHandler<AtmosphereChangedEventArgs> AtmosphereChanged;
     }
 
+
     public class AtmosphereModule : IAtmosphereModule, IDisposable
     {
+        private readonly ConcurrentDictionary<AtmosphereScenario, MediaSource> _atmosphereCache;
+        private MediaPlayer _currentPlayer;
+
+        private float _moduleVolume = 1.0f;
+        private float _masterVolume = 1.0f;
+        private bool _isInitialized = false;
+
+        // IAudioModule Properties
+        public string ModuleName => "Atmosphere";
+        public AudioModuleType ModuleType => AudioModuleType.Atmosphere;
+
+        // IAtmosphereModule Properties
+        public AtmosphereScenario CurrentScenario { get; private set; } = AtmosphereScenario.None;
+        public bool IsPlaying => _currentPlayer?.PlaybackSession?.PlaybackState == MediaPlaybackState.Playing;
+        public bool IsEnabled { get; set; } = true;
+
+        // Events
+        public event EventHandler<AudioModuleEventArgs> StatusChanged;
+        public event EventHandler<AtmosphereChangedEventArgs> AtmosphereChanged;
+
+
+        public AtmosphereModule()
+        {
+            _atmosphereCache = new ConcurrentDictionary<AtmosphereScenario, MediaSource>();
+        }
+
     }
 }

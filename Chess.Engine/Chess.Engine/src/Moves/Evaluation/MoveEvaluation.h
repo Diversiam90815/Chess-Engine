b95d338183dbd769884472d3e488bbe731bdbdcd
Jens Langenberg <jens.langenberg@corsair.com>
2025-07-26 11:24:09 +0200

    Added more dummy (not impl) methods for more tactical MoveEvaluation

--- Chess.Engine/Chess.Engine/src/Moves/Evaluation/MoveEvaluation.h
+++ Chess.Engine/Chess.Engine/src/Moves/Evaluation/MoveEvaluation.h
@@ -12,6 +12,13 @@
 #include "Move.h"
 #include "ChessBoard.h"
 
+enum class GamePhase
+{
+	Opening	   = 1,
+	MiddleGame = 2,
+	EndGame	   = 3
+};
+
 
 class MoveEvaluation
 {
@@ -20,81 +27,117 @@ public:
 	~MoveEvaluation() = default;
 
 	int					 getBasicEvaluation(const PossibleMove &move);
-
 	int					 getMediumEvaluation(const PossibleMove &move, PlayerColor player);
-
 	int					 getAdvancedEvaluation(const PossibleMove &move, PlayerColor player);
 
 	static constexpr int getPieceValue(PieceType piece);
-
 	int					 getPositionValue(PieceType piece, const Position &pos, PlayerColor player);
-
 	int					 evaluateMaterialGain(const PossibleMove &move);
-
 	int					 evaluatePositionalGain(const PossibleMove &move, PlayerColor player);
+	int					 evaluateThreadLevel(const PossibleMove &move, PlayerColor player);
+	int					 evaluateKingSafety(const PossibleMove &move, PlayerColor player);
+	int					 evaluateCenterControl(const PossibleMove &move, PlayerColor player);
+	int					 evaluatePawnStructure(const PossibleMove &move, PlayerColor player);
+	int					 evaluatePieceActivity(const PossibleMove &move, PlayerColor player);
+	int					 evaluateDefensivePatterns(const PossibleMove &move, PlayerColor player);
+
+	int					 getStrategicEvaluation(const PossibleMove &move, PlayerColor player);
+	int					 getTacticalEvaluation(const PossibleMove &move, PlayerColor player);
+
+	GamePhase			 determineGamePhase() const;
 
 
 private:
+	int												   calculateMobility(PlayerColor player) const;
+	int												   calculateKingSafetyScore(PlayerColor player) const;
+	int												   calculatePawnStructureScore(PlayerColor player) const;
+	bool											   isPasssedPawn(const Position &pos, PlayerColor player) const;
+	bool											   isIsolatedPawn(const Position &pos, PlayerColor player) const;
+	bool											   isDoublePawn(const Position &pos, PlayerColor player) const;
+	bool											   isInCenter(const Position &pos) const;
+	bool											   isNearKing(const Position &pos) const;
+	std::vector<Position>							   getAttackedSquares(const Position &piecePos, PlayerColor player) const;
+	bool											   wouldExposeKing(const PossibleMove &move, PlayerColor player) const;
+	int												   countAttackers(const Position &target, PlayerColor attackerPlayer) const;
+	PlayerColor										   getOpponnentColor(PlayerColor player) const;
+
+
 	std::shared_ptr<ChessBoard>						   mBoard;
 
-	static constexpr int							   PAWN_VALUE	= 100;
-	static constexpr int							   KNIGHT_VALUE = 320;
-	static constexpr int							   BISHOP_VALUE = 330;
-	static constexpr int							   ROOK_VALUE	= 500;
-	static constexpr int							   QUEEN_VALUE	= 900;
+	static constexpr int							   PAWN_VALUE			   = 100;
+	static constexpr int							   KNIGHT_VALUE			   = 320;
+	static constexpr int							   BISHOP_VALUE			   = 330;
+	static constexpr int							   ROOK_VALUE			   = 500;
+	static constexpr int							   QUEEN_VALUE			   = 900;
+
+	static constexpr int							   CHECKMATE_BONUS		   = 10000;
+	static constexpr int							   CHECK_BONUS			   = 50;
+	static constexpr int							   CAPTURE_BONUS		   = 25;
+	static constexpr int							   CASTLE_BONUS			   = 60;
+	static constexpr int							   PROMOTION_BONUS		   = 800;
+	static constexpr int							   CENTER_CONTROL_BONUS	   = 15;
+	static constexpr int							   KING_SAFETY_WEIGHT	   = 3;
+	static constexpr int							   MOBILITY_WEIGHT		   = 2;
+	static constexpr int							   THREAT_WEIGHT		   = 4;
+
+	static constexpr int							   FORK_BONUS			   = 150;
+	static constexpr int							   PIN_BONUS			   = 100;
+	static constexpr int							   SKEWER_BONUS			   = 120;
+	static constexpr int							   DISCOVERED_ATTACK_BONUS = 80;
+	static constexpr int							   THREAT_BLOCK_BONUS	   = 60;
 
 	// Position value tables (from white's perspective, flip for black)
-	static constexpr std::array<std::array<int, 8>, 8> PAWN_TABLE	= {{{0, 0, 0, 0, 0, 0, 0, 0},
-																		{50, 50, 50, 50, 50, 50, 50, 50},
-																		{10, 10, 20, 30, 30, 20, 10, 10},
-																		{5, 5, 10, 25, 25, 10, 5, 5},
-																		{0, 0, 0, 20, 20, 0, 0, 0},
-																		{5, -5, -10, 0, 0, -10, -5, 5},
-																		{5, 10, 10, -20, -20, 10, 10, 5},
-																		{0, 0, 0, 0, 0, 0, 0, 0}}};
+	static constexpr std::array<std::array<int, 8>, 8> PAWN_TABLE			   = {{{0, 0, 0, 0, 0, 0, 0, 0},
+																				   {50, 50, 50, 50, 50, 50, 50, 50},
+																				   {10, 10, 20, 30, 30, 20, 10, 10},
+																				   {5, 5, 10, 25, 25, 10, 5, 5},
+																				   {0, 0, 0, 20, 20, 0, 0, 0},
+																				   {5, -5, -10, 0, 0, -10, -5, 5},
+																				   {5, 10, 10, -20, -20, 10, 10, 5},
+																				   {0, 0, 0, 0, 0, 0, 0, 0}}};
 
-	static constexpr std::array<std::array<int, 8>, 8> KNIGHT_TABLE = {{{-50, -40, -30, -30, -30, -30, -40, -50},
-																		{-40, -20, 0, 0, 0, 0, -20, -40},
-																		{-30, 0, 10, 15, 15, 10, 0, -30},
-																		{-30, 5, 15, 20, 20, 15, 5, -30},
-																		{-30, 0, 15, 20, 20, 15, 0, -30},
-																		{-30, 5, 10, 15, 15, 10, 5, -30},
-																		{-40, -20, 0, 5, 5, 0, -20, -40},
-																		{-50, -40, -30, -30, -30, -30, -40, -50}}};
+	static constexpr std::array<std::array<int, 8>, 8> KNIGHT_TABLE			   = {{{-50, -40, -30, -30, -30, -30, -40, -50},
+																				   {-40, -20, 0, 0, 0, 0, -20, -40},
+																				   {-30, 0, 10, 15, 15, 10, 0, -30},
+																				   {-30, 5, 15, 20, 20, 15, 5, -30},
+																				   {-30, 0, 15, 20, 20, 15, 0, -30},
+																				   {-30, 5, 10, 15, 15, 10, 5, -30},
+																				   {-40, -20, 0, 5, 5, 0, -20, -40},
+																				   {-50, -40, -30, -30, -30, -30, -40, -50}}};
 
-	static constexpr std::array<std::array<int, 8>, 8> BISHOP_TABLE = {{{-20, -10, -10, -10, -10, -10, -10, -20},
-																		{-10, 0, 0, 0, 0, 0, 0, -10},
-																		{-10, 0, 5, 10, 10, 5, 0, -10},
-																		{-10, 5, 5, 10, 10, 5, 5, -10},
-																		{-10, 0, 10, 10, 10, 10, 0, -10},
-																		{-10, 10, 10, 10, 10, 10, 10, -10},
-																		{-10, 5, 0, 0, 0, 0, 5, -10},
-																		{-20, -10, -10, -10, -10, -10, -10, -20}}};
+	static constexpr std::array<std::array<int, 8>, 8> BISHOP_TABLE			   = {{{-20, -10, -10, -10, -10, -10, -10, -20},
+																				   {-10, 0, 0, 0, 0, 0, 0, -10},
+																				   {-10, 0, 5, 10, 10, 5, 0, -10},
+																				   {-10, 5, 5, 10, 10, 5, 5, -10},
+																				   {-10, 0, 10, 10, 10, 10, 0, -10},
+																				   {-10, 10, 10, 10, 10, 10, 10, -10},
+																				   {-10, 5, 0, 0, 0, 0, 5, -10},
+																				   {-20, -10, -10, -10, -10, -10, -10, -20}}};
 
-	static constexpr std::array<std::array<int, 8>, 8> ROOK_TABLE	= {{{0, 0, 0, 0, 0, 0, 0, 0},
-																		{5, 10, 10, 10, 10, 10, 10, 5},
-																		{-5, 0, 0, 0, 0, 0, 0, -5},
-																		{-5, 0, 0, 0, 0, 0, 0, -5},
-																		{-5, 0, 0, 0, 0, 0, 0, -5},
-																		{-5, 0, 0, 0, 0, 0, 0, -5},
-																		{-5, 0, 0, 0, 0, 0, 0, -5},
-																		{0, 0, 0, 5, 5, 0, 0, 0}}};
+	static constexpr std::array<std::array<int, 8>, 8> ROOK_TABLE			   = {{{0, 0, 0, 0, 0, 0, 0, 0},
+																				   {5, 10, 10, 10, 10, 10, 10, 5},
+																				   {-5, 0, 0, 0, 0, 0, 0, -5},
+																				   {-5, 0, 0, 0, 0, 0, 0, -5},
+																				   {-5, 0, 0, 0, 0, 0, 0, -5},
+																				   {-5, 0, 0, 0, 0, 0, 0, -5},
+																				   {-5, 0, 0, 0, 0, 0, 0, -5},
+																				   {0, 0, 0, 5, 5, 0, 0, 0}}};
 
-	static constexpr std::array<std::array<int, 8>, 8> QUEEN_TABLE	= {{{-20, -10, -10, -5, -5, -10, -10, -20},
-																		{-10, 0, 0, 0, 0, 0, 0, -10},
-																		{-10, 0, 5, 5, 5, 5, 0, -10},
-																		{-5, 0, 5, 5, 5, 5, 0, -5},
-																		{0, 0, 5, 5, 5, 5, 0, -5},
-																		{-10, 5, 5, 5, 5, 5, 0, -10},
-																		{-10, 0, 5, 0, 0, 0, 0, -10},
-																		{-20, -10, -10, -5, -5, -10, -10, -20}}};
+	static constexpr std::array<std::array<int, 8>, 8> QUEEN_TABLE			   = {{{-20, -10, -10, -5, -5, -10, -10, -20},
+																				   {-10, 0, 0, 0, 0, 0, 0, -10},
+																				   {-10, 0, 5, 5, 5, 5, 0, -10},
+																				   {-5, 0, 5, 5, 5, 5, 0, -5},
+																				   {0, 0, 5, 5, 5, 5, 0, -5},
+																				   {-10, 5, 5, 5, 5, 5, 0, -10},
+																				   {-10, 0, 5, 0, 0, 0, 0, -10},
+																				   {-20, -10, -10, -5, -5, -10, -10, -20}}};
 
-	static constexpr std::array<std::array<int, 8>, 8> KING_TABLE	= {{{-30, -40, -40, -50, -50, -40, -40, -30},
-																		{-30, -40, -40, -50, -50, -40, -40, -30},
-																		{-30, -40, -40, -50, -50, -40, -40, -30},
-																		{-30, -40, -40, -50, -50, -40, -40, -30},
-																		{-20, -30, -30, -40, -40, -30, -30, -20},
-																		{-10, -20, -20, -20, -20, -20, -20, -10},
-																		{20, 20, 0, 0, 0, 0, 20, 20},
-																		{20, 30, 10, 0, 0, 10, 30, 20}}};
+	static constexpr std::array<std::array<int, 8>, 8> KING_TABLE			   = {{{-30, -40, -40, -50, -50, -40, -40, -30},
+																				   {-30, -40, -40, -50, -50, -40, -40, -30},
+																				   {-30, -40, -40, -50, -50, -40, -40, -30},
+																				   {-30, -40, -40, -50, -50, -40, -40, -30},
+																				   {-20, -30, -30, -40, -40, -30, -30, -20},
+																				   {-10, -20, -20, -20, -20, -20, -20, -10},
+																				   {20, 20, 0, 0, 0, 0, 20, 20},
+																				   {20, 30, 10, 0, 0, 10, 30, 20}}};
 };

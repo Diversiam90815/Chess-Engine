b3444369173a26f18c249f4521c77b56b30247c3
Jens Langenberg <jens.langenberg@corsair.com>
2025-12-14 16:18:37 +0100

    Added Bitboard (empty class) and helper macros

---
+++ Chess.Engine/src/Chess.Engine.Core/src/Board/Bitboard.h
@@ -0,0 +1,11 @@
+/*
+  ==============================================================================
+	Module:         Bitboard
+	Description:    A bitboard representing a chess board
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "BitboardHelper.h"
+

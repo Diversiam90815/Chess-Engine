3f4fe57048d094291d89ea03327df3a577203232
Jens W. Langenberg <william@diversiam.com>
2026-01-03 15:26:22 +0100

    Minor changes

--- Chess.Engine/src/Chess.Engine.Core/src/Moves/Execution/MoveExecution.h
+++ Chess.Engine/src/Chess.Engine.Core/src/Moves/Execution/MoveExecution.h
@@ -45,9 +45,18 @@ private:
 
 	std::vector<MoveHistoryEntry> mHistory;
 
-
 	// clang-format off
-	// Castling rights update table
+	/*
+				Castling rights update table
+
+	a1	(white queenside rook)		7		0111	Clears WQ bit 
+	h1	(white kingside rook)		11		1011	Clears WK bit 
+	e1	(white king)				3		0011	Clears both WK and WQ
+	a8	(black queenside rook)		13		1101	Clears BQ bit 
+	h8	(black kingside rook)		14		1110	Clears BK bit 
+	e8	(black king)				12		1100	Clears both BK and BQ 
+	All other squares				15		1111	No change
+	*/
 	static constexpr uint8_t	  sCastlingRightsUpdate[64] = 
 	{
 		 13, 15, 15, 15, 12, 15, 15, 14,	// a8-h8

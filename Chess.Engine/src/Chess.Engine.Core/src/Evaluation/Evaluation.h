31adb89c6ecc9f4f788224ee9cdb81262468b06b
Jens W. Langenberg <william@diversiam.com>
2026-02-22 20:08:51 +0100

    Added minimal Evaluation class (TODO)

---
+++ Chess.Engine/src/Chess.Engine.Core/src/Evaluation/Evaluation.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+	Module:         Evaluation
+	Description:    Positional Evaluation of the chess board
+  ==============================================================================
+*/
+
+
+#pragma once
+
+#include "Chessboard.h"
+
+
+class Evaluation
+{
+	Evaluation()  = default;
+	~Evaluation() = default;
+
+
+	// TODO
+};

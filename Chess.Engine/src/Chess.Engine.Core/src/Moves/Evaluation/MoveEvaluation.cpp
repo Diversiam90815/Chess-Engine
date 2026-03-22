79e98fd89354ff6f142c39482e80842d8050ddf1
Jens W Langenberg <jerry2912@icloud.com>
2026-03-22 13:55:35 +0100

    Added more MoveEvaluation

--- Chess.Engine/src/Chess.Engine.Core/src/Moves/Evaluation/MoveEvaluation.cpp
+++ Chess.Engine/src/Chess.Engine.Core/src/Moves/Evaluation/MoveEvaluation.cpp
@@ -37,37 +37,124 @@ void MoveEvaluation::orderMoves(MoveList &moves, const Chessboard &board, Move t
 }
 
 
-void MoveEvaluation::orderCaptures(MoveList &moves, const Chessboard &board) const {}
-
-
-void MoveEvaluation::updateKillerMove(Move move, int ply) {}
-
-
-void MoveEvaluation::updateHistory(Move move, int depth) {}
-
-
-void MoveEvaluation::clearSearchState() {}
-
-
-int	 MoveEvaluation::evaluateMove(Move move, const Chessboard &board, Move ttMove, int ply) const
+void MoveEvaluation::orderCaptures(MoveList &moves, const Chessboard &board) const
 {
-	return 0;
+	for (size_t i = 0; i < moves.size(); ++i)
+	{
+		size_t bestIdx	 = i;
+		int	   bestScore = evaluateMVV_LVA(moves[i], board);
+
+		for (size_t j = i + 1; j < moves.size(); ++j)
+		{
+			int s = evaluateMVV_LVA(moves[j], board);
+
+			if (s > bestScore)
+			{
+				bestScore = s;
+				bestIdx	  = j;
+			}
+		}
+
+		if (bestIdx != i)
+			std::swap(moves[i], moves[bestIdx]);
+	}
+}
+
+
+void MoveEvaluation::updateKillerMove(Move move, int ply)
+{
+	if (ply < 0 || ply >= MAX_PLY)
+		return;
+
+	// do not store captures as killers (only quiet moves)
+	if (move.isCapture())
+		return;
+
+	// shift: slot 0 -> slot 1, then store slot 0
+	if (mKillers[ply][0] != move)
+	{
+		mKillers[ply][1] = mKillers[ply][0];
+		mKillers[ply][0] = move;
+	}
+}
+
+
+void MoveEvaluation::updateHistory(Move move, int depth)
+{
+	if (move.isCapture())
+		return;
+
+	int from = to_index(move.from());
+	int to	 = to_index(move.to());
+
+	// depth-squared bonus (deeper cutoffs are more valuable)
+	mHistory[from][to] += depth * depth;
+
+	// prevent overflow by scaling all entries
+	if (mHistory[from][to] > 1'000'000)
+	{
+		for (auto &row : mHistory)
+			for (auto &val : row)
+				val /= 2;
+	}
+}
+
+
+void MoveEvaluation::clearSearchState()
+{
+	for (auto &ply : mKillers)
+		ply.fill(Move::none());
+
+	std::memset(mHistory, 0, sizeof(mHistory));
+}
+
+
+int MoveEvaluation::evaluateMove(Move move, const Chessboard &board, Move ttMove, int ply) const
+{
+	// 1 TT best move (always search first)
+	if (ttMove.isValid() && move == ttMove)
+		return SCORE_TT_MOVE;
+
+	// 2 Captures - MVV-LVA
+	if (move.isCapture())
+		return SCORE_CAPTURE + evaluateMVV_LVA(move, board);
+
+	// 3 promotions
+	if (move.isPromotion())
+		return SCORE_PROMOTION;
+
+	// 4 killer moves
+	if (isKillerMove(move, ply))
+		return (move == mKillers[ply][0]) ? SCORE_KILLER_1 : SCORE_KILLER_2;
+
+	// 5 history heuristic
+	return getHistoryScore(move);
 }
 
 
 int MoveEvaluation::evaluateMVV_LVA(Move move, const Chessboard &board)
 {
-	return 0;
+	PieceType victim   = board.pieceAt(move.to());
+	PieceType attacker = board.pieceAt(move.from());
+
+	if (victim == PieceType::None || attacker == PieceType::None)
+		return 0;
+
+	// High victim value + low attacker value = best capture
+	return PieceValues::BY_TYPE[victim] * 10 - PieceValues::BY_TYPE[attacker];
 }
 
 
 bool MoveEvaluation::isKillerMove(Move move, int ply) const
 {
-	return false;
+	if (ply < 0 || ply >= MAX_PLY)
+		return false;
+
+	return (move == mKillers[ply][0]) || (move == mKillers[ply][1]);
 }
 
 
 int MoveEvaluation::getHistoryScore(Move move) const
 {
-	return 0;
+	return mHistory[to_index(move.from())][to_index(move.to())];
 }
